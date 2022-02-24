d6d020a86486133c294117ef618492c639f133d5
hadymohamedmohamedmorsy <hady812012@gmai.com>
2022-02-24 13:54:21 +0200

    add

---
+++ inc/footer.php
@@ -0,0 +1,34 @@
+    <!-- footer -->
+    <div class="footer pt-5 color-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <?php  foote_first(); ?>
+                    </div>
+                    <div class="col-lg-3">
+                        <?php footer_secound(); ?>
+                    </div>
+                    <div class="col-lg-6 right_text">
+                        <p class="Adress">21 Wadi ELNIL STREET ,AGOUZA DISTRICT GIZA 12411</p>
+                        <p class="email"> <a href="mailto:info@bonappeti.com.eg">INFO@BONAPPETI.COM.EG </a> </p>
+                        <p class="number"> <a href="tel:19101">19101</a></p>
+                    </div>
+                    <div class="social-media text-center w-100">
+                        <a href="">
+                            <img src="<?php echo get_theme_file_uri('/assits/images/facbook.png') ?>" alt="<?php echo esc_attr(get_bloginfo( 'name' )); ?>">
+                        </a>
+                        <a href="">
+                            <img  src="<?php echo get_theme_file_uri('/assits/images/instegram.png') ?>" alt="<?php echo esc_attr(get_bloginfo( 'name' )); ?>">
+                        </a>
+                        <a href="">
+                            <img src="<?php echo get_theme_file_uri('/assits/images/tiktoke.png') ?>" alt="<?php echo esc_attr(get_bloginfo( 'name' )); ?>">
+                        </a>
+                        <a href="">
+                            <img src="<?php echo get_theme_file_uri('/assits/images/youtupe.png') ?>" alt="<?php echo esc_attr(get_bloginfo( 'name' )); ?>">
+                        </a>
+                        <p>Bonapettit @ 2022 All</p>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <!-- footer -->
---
+++ inc/nav.php
@@ -0,0 +1,28 @@
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand logo" href="#"> 
+                <!-- logo is here -->
+                <?php
+                    $custom_logo_id = get_theme_mod( 'custom_logo' );
+                    $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+                        
+                        if ( has_custom_logo() ) {
+                            echo '<img src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '">';
+                        } else {
+                            echo '<h1>' . get_bloginfo('name') . '</h1>';
+                        }
+                ?>
+            <!-- logo is here -->
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- links is here -->
+            <?php nav(); ?>
+                <!-- links is here -->
+            </div>
+        </div>
+    </nav>
+    <!-- navbar -->
